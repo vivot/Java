5f55275f3cb8dc4c2c0a1bb4f3ecb901ae0ad28c
Vi Vo <vothuyvi5693@gmail.com>
2023-10-24 13:39:25 +0700

    lab 9- updated

--- src/lab9/Animal.java
+++ src/lab9/Animal.java
@@ -4,23 +4,53 @@ import java.security.SecureRandom;
 
 public class Animal {
     protected int randomSpeed = new SecureRandom().nextInt();
-    protected String animalName;
+    protected String name;
     protected boolean hasWings;
 
-    protected Animal() {
+    private Animal(Builder builder) {
+        this.name = builder.name;
+        this.randomSpeed = builder.randomSpeed;
+        this.hasWings = builder.hasWings;
+    }
 
+    public String getName() {
+        return name;
     }
 
     public int getRandomSpeed() {
         return randomSpeed;
     }
 
-    public String getAnimalName() {
-        return "Animal";
+    public boolean hasWings() {
+        return hasWings;
     }
 
-    private void setRandomSpeed(int randomSpeed) {
-        this.randomSpeed = randomSpeed;
-    }
 
+    public static class Builder {
+        private String name = "Animal";
+        private int randomSpeed = new SecureRandom().nextInt( 120 );
+        private boolean hasWings = false;
+
+
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder setRandomSpeed(int randomSpeed) {
+            this.randomSpeed = randomSpeed;
+            return this;
+        }
+
+        public Builder setHasWings(boolean hasWings) {
+            this.hasWings = hasWings;
+            return this;
+        }
+
+        public Animal build() {
+            return new Animal( this ) {
+
+            };
+        }
+    }
 }
--- src/lab9/AnimalWithBuilderTest.java
+++ src/lab9/AnimalWithBuilderTest.java
@@ -3,22 +3,32 @@ package lab9;
 import java.util.ArrayList;
 import java.util.List;
 
-import static lab9.AnimalWithBuilder.Builder;
-
 public class AnimalWithBuilderTest {
 
     public static void main(String[] args) {
-        Builder builder = new Builder();
-        List<Animal> animalList = new ArrayList<Animal>();
-        Animal animal = builder.build();
-        animalList.add( new Dog() );
-        animalList.add( new Tiger() );
-        animalList.add( new Eagle() );
-        animalList.add( new Horse() );
-        animalList.add( new Falcon() );
-        List<Animal> filterAnimal = AnimalController.filterAnimal( animalList );
-        Animal winner = AnimalController.findWinner( filterAnimal );
-        System.out.println( "Winner is: " + winner.getAnimalName() + " with speed: " + winner.getRandomSpeed() + " has wings is " + winner.hasWings );
+        List<Animal> animalList=new ArrayList<>();
+        Animal tiger = new Animal.Builder().setName("tiger").setRandomSpeed(10).setHasWings(false).build();
+        Animal dog = new Animal.Builder().setName("dog").setRandomSpeed(60).setHasWings(false).build();
+        Animal eagle = new Animal.Builder().setName("eagle").setRandomSpeed(100).setHasWings(true).build();
+        Animal falcon = new Animal.Builder().setName("falcon").setRandomSpeed(100).setHasWings(true).build();
+        Animal horse = new Animal.Builder().setName("horse").setRandomSpeed(75).setHasWings(true).build();
+
+        animalList.add( tiger );
+        animalList.add( dog );
+        animalList.add( eagle );
+        animalList.add( falcon );
+        animalList.add( horse );
+
+        List<Animal> filterAnimal =AnimalController.filterAnimal( animalList );
+        Animal winner =AnimalController.findWinner( filterAnimal );
+        System.out.println("Winner is: "+winner.getName()+ "\nWith speed: "+winner.getRandomSpeed());
+
+
+
+
+
+
+
 
     }
 }
